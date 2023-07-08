94a946ade299f54ea9f7b71c277cb845c0bef5dd
anasalmezyd <Anas.almziad@gmail.com>
2023-07-08 08:25:06 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+function submitData(name, email) {
+    const formData = {
+      name: name,
+      email: email
+    };
+  
+    const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+    };
+  
+    return fetch("http://localhost:3000/users", configurationObject)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(object) {
+        const id = object.id;
+        const idElement = document.createElement('p');
+        idElement.textContent = "New User ID: " + id;
+        document.body.appendChild(idElement);
+      })
+      .catch(function(error) {
+        const errorMessage = error.message;
+        const errorElement = document.createElement('p');
+        errorElement.textContent = "Error: " + errorMessage;
+        document.body.appendChild(errorElement);
+      });
+  }
+  
